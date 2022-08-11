f838a325e34aef0a9fe7d00e238746756882fe39
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-08-11 03:18:10 -0700

    simple shell

---
+++ execute.c
@@ -0,0 +1,47 @@
+#include "shell.h"
+
+/**
+ * execute - execute a command with its entire path variables
+ * @data: a pointer to the program's data
+ * Return: If sucess returns zero, otherwise, return -1.
+ */
+
+int execute(data_of_program *data)
+{
+	int retval = 0, status;
+	pid_t pidd;
+
+	retval = builtins_list(data);
+	if (retval != -1)/* if program was found in built ins */
+		return (retval);
+
+	retval = find_program(data);
+	if (retval)
+	{
+		return (retval);
+	}
+	else
+	{
+		pidd = fork(); /* create a child process */
+		if (pidd == -1)
+		{
+			perror(data->command_name);
+			exit(EXIT_FAILURE);
+		}
+		if (pidd == 0)
+		{
+			retval = execve(data->tokens[0], data->tokens, data->env);
+			if (retval == -1) /* if error when execve*/
+				perror(data->command_name), exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(&status);
+			if (WIFEXITED(status))
+				errno = WEXITSTATUS(status);
+			else if (WIFSIGNALED(status))
+				errno = 128 + WTERMSIG(status);
+		}
+	}
+	return (0);
+}
